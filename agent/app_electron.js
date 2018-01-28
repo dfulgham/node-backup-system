e35037f9c5a32e96d7e7f1e60cc7db74975b7a7b
David Fulgham <user@veronique-pc.dir.nhc.zone>
2018-01-28 18:48:44 -0500

    some work on schedules and other resource documents

--- agent/app_electron.js
+++ agent/app_electron.js
@@ -1,10 +1,113 @@
 (function() {
     'use strict';
 
-    // Headless Agent
+    // ELECTRON Stuff
+
+    const Electron = require('electron');
+
+  const {app, BrowserWindow} = require('electron')
+  const {Menu, Tray} = require('electron')
+  const nativeImage = require('electron').nativeImage
+  let tray = null;
+  let mainWindow = null;
+
+  //require('electron-debug')({showDevTools: true});
+
+    // Keep a global reference of the window object, if you don't, the window will
+    // be closed automatically when the JavaScript object is garbage collected.
+
+
+    // Quit when all windows are closed.
+    app.on('window-all-closed', function() {
+      // On OS X it is common for applications and their menu bar
+      // to stay active until the user quits explicitly with Cmd + Q
+      if (process.platform != 'darwin') {
+        app.quit();
+      }
+    });
+
+    // This method will be called when Electron has finished
+    // initialization and is ready to create browser windows.
+    app.on('ready', function() {
+
+
+        let image = nativeImage.createFromPath('images/icon.png')
+
+
+
+      tray = new Tray('images/icon.png')
+      const contextMenu = Menu.buildFromTemplate([
+        {label: 'Item1', type: 'radio'},
+        {label: 'Item2', type: 'radio'},
+        {label: 'Item3', type: 'radio', checked: true},
+        {label: 'Item4', type: 'radio'}
+      ])
+      tray.setToolTip('This is my application.')
+      //tray.setContextMenu(contextMenu)
+      tray.on('click', buildWindow)
+
+
+
+      function buildWindow(){
+
+        var cursorPosition = Electron.screen.getCursorScreenPoint();
+
+
+        console.log("tray clicked")
+        // Create the browser window.
+       if(!mainWindow)  {
+         mainWindow = new BrowserWindow({
+          x: cursorPosition.x - 400,
+          y: 40,
+          frame: false,
+          transparent: true,
+          width: 800,
+          height: 400,
+          MinWidth: 500,
+          MinHeight: 200,
+          resizable: false,
+          // 'accept-first-mouse': true,
+          // 'title-bar-style': 'hidden'
+        })
+        //mainWindow.on('blur',buildWindow)
+        // Emitted when the window is closed.
+        mainWindow.on('closed', function() {
+          // Dereference the window object, usually you would store windows
+          // in an array if your app supports multi windows, this is the time
+          // when you should delete the corresponding element.
+          mainWindow = null;
+        });
+        mainWindow.loadURL('file://' + __dirname + '/index.html');
+      }
+        else {
+          if(mainWindow.isVisible()) mainWindow.hide()
+          else mainWindow.show()
+        }
+
+      }
+
+
+      // and load the index.html of the app.
+      //mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+      // Open the DevTools.
+      //mainWindow.openDevTools();
+
+
+    });
+
+
+
+
+    // End Electron Stuff
+
+
+
+
+
 
     var schedule = require('node-schedule');
-    var nano = require('nano')('http://localhost:5984/node_backup');
+    var nano = require('nano')('http://10.10.0.200:5984/node_backup');
     const uuid = require('uuid/v1');
     var fs = require('fs')
     var os = require('os')
@@ -55,8 +158,6 @@
               fs.writeFile("./config", JSON.stringify(docs), function(err) {
                 // console.log("changes applied", err)
               })
-              // update data to match docs
-              data = docs;
             }
           })
 
@@ -72,20 +173,15 @@
 
             }
             configChanges.on('change', function(change) {
-              console.log("change: ", change);
+              // console.log("change: ", change);
               //write config to disk
               fs.writeFile("./config", JSON.stringify(change.doc), function(err) {
-                console.log("config to file:", err)
+                // console.log("changes applied", err)
               })
-              LoadResources(data);
 
             })
 
             configChanges.follow();
-
-            LoadResources(data);
-
-
         }
       })
   }
@@ -111,19 +207,15 @@
 
         }
         configChanges.on('change', function(change) {
-          console.log("config change: ", change);
+          // console.log("change: ", change);
           //write config to disk
           fs.writeFile("./config", JSON.stringify(change.doc), function(err) {
-             console.log("config to file:", err)
+            // console.log("changes applied", err)
           })
-          LoadResources(docs);
-
 
         })
 
         configChanges.follow();
-
-        LoadResources(docs);
 
     })
 
@@ -131,18 +223,6 @@
 
 
   });
-  }
-
-
-  // Load host LoadResources
-  function LoadResources(host){
-    console.log(host)
-    host.jobs.forEach(function(job){
-      console.log(job);
-
-    })
-
-
   }
 
 }());
